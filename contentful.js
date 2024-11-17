c4e6a8571e63af63f9ee75fd62abaa43d35399c4
hiruy4004 <hiruy4004@gmail.com>
2024-11-17 19:17:03 +0300

    Add files via upload

--- contentful.js
+++ contentful.js
@@ -5,9 +5,6 @@ const client = createClient({
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
-
-import client from './contentful';
-
 export async function getStaticProps() {
     const entries = await client.getEntries({ content_type: 'yourContentType' });
     return {
@@ -25,4 +22,27 @@ export default function Home({ entries }) {
             ))}
         </div>
     );
+}
+import Image from 'next/image';
+
+export default function Home({ entries }) {
+  return (
+    <div>
+      {entries.map((entry) => (
+        <div key={entry.sys.id}>
+          <h1>{entry.fields.title}</h1>
+          <p>{entry.fields.description}</p>
+          {entry.fields.image && (
+            <Image
+              src={`https:${entry.fields.image.fields.file.url}`} // Prepend 'https:'
+              alt={entry.fields.title}
+              width={600} // Adjust width
+              height={400} // Adjust height
+              priority // Optimizes image loading
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
